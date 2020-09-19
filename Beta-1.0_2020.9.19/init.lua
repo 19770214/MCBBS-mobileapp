8b8c60e0783c466e6a518522f5022600ba83ecf8
19770214 <53890138+19770214@users.noreply.github.com>
2020-09-19 11:14:19 +0800

    Beta-1.0
    2020.9.19更新的第一个版本
    版本号:Beta-1.0
    欢迎各个大佬指点小弟的软件awa

---
+++ Beta-1.0_2020.9.19/init.lua
@@ -0,0 +1,15 @@
+packagename="com.myc.MCBBS"
+appver="β-1.0"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appname="MCBBS"
+template="bottom"
+appcode="1"
+
+--require "import" import"loadlayout" import"loadmenu" import"loadbitmap" 
